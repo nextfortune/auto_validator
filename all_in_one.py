cacd88f76d29f17f251d90cd9a10f1d8b089c762
nf-jimmysu <jimmy.su@nextfortune.tw>
2021-11-11 10:13:02 +0800

    upload version for pyspark reader setting

--- all_in_one.py
+++ all_in_one.py
@@ -1,3 +1,5 @@
+import os
+import fnmatch
 from ruamel import yaml
 
 import great_expectations as ge
@@ -14,7 +16,7 @@ assert context
 
 # First configure a new Datasource and add to DataContext
 datasource_name = "all_in_one_test"
-datasource_path = "/data"
+datasource_path = "/home/sid/projects/ge_test/data/"
 
 datasource_yaml = f"""
 name: {datasource_name}
@@ -49,7 +51,9 @@ batch_request = BatchRequest(
     data_connector_name="default_inferred_data_connector_name",
     data_asset_name=example_file_name ,
     limit=1000,
+    batch_spec_passthrough={"reader_method": "csv", "reader_options": {"header":True, "inferSchema":True}},
 )
+#above batch_spec_passthrough optional for pyspark reader setting
 context.create_expectation_suite(expectation_suite_name=expectation_suite_name)
 validator = context.get_validator(
     batch_request=batch_request,
@@ -60,24 +64,24 @@ assert isinstance(validator, Validator)
 
 # Profile the data with the UserConfigurableProfiler and save resulting ExpectationSuite
 ignored_columns = [
-    "vendor_id",
-    "pickup_datetime",
-    "dropoff_datetime",
-    # "passenger_count",
-    "trip_distance",
-    "rate_code_id",
-    "store_and_fwd_flag",
-    "pickup_location_id",
-    "dropoff_location_id",
-    "payment_type",
-    "fare_amount",
-    "extra",
-    "mta_tax",
-    "tip_amount",
-    "tolls_amount",
-    "improvement_surcharge",
-    "total_amount",
-    "congestion_surcharge",
+    # "vendor_id",
+    # "pickup_datetime",
+    # "dropoff_datetime",
+    # # "passenger_count",
+    # "trip_distance",
+    # "rate_code_id",
+    # "store_and_fwd_flag",
+    # "pickup_location_id",
+    # "dropoff_location_id",
+    # "payment_type",
+    # "fare_amount",
+    # "extra",
+    # "mta_tax",
+    # "tip_amount",
+    # "tolls_amount",
+    # "improvement_surcharge",
+    # "total_amount",
+    # "congestion_surcharge",
 ]
 
 profiler = UserConfigurableProfiler(
@@ -98,7 +102,7 @@ my_checkpoint_config = f"""
 name: checkpoint on example file
 config_version: 1.0
 class_name: SimpleCheckpoint
-run_name_template: "%Y%m%d-%H%M%S-checkpoint-for-example"
+run_name_template: "%Y%m%d-%H%M%S-check-suite"
 validations:
   - batch_request:
       datasource_name: {datasource_name }
@@ -106,8 +110,14 @@ validations:
       data_asset_name: {example_file_name }
       data_connector_query:
         index: -1
+      batch_spec_passthrough: 
+        reader_method: csv
+        reader_options: 
+          header: True
+          inferSchema: True
     expectation_suite_name: {expectation_suite_name }
 """
+#above batch_spec_passthrough optional for header and schema
 my_checkpoint_config = yaml.load(my_checkpoint_config)
 
 # NOTE: The following code (up to and including the assert) is only for testing and can be ignored by users.
@@ -119,33 +129,52 @@ checkpoint = SimpleCheckpoint(
 checkpoint_result = checkpoint.run()
 assert checkpoint_result.run_results
 
+#get all filename in path
+def files(path):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            yield file
 
-# Create second checkpoint on testing file
-testing_file_name = "yellow_tripdata_sample_2019-02.csv"
-my_new_checkpoint_config = f"""
-name: checkpoint on testing file
-config_version: 1.0
-class_name: SimpleCheckpoint
-run_name_template: "%Y%m%d-%H%M%S-checkpoint-for-testing-file"
-validations:
-  - batch_request:
-      datasource_name: {datasource_name }
-      data_connector_name: default_inferred_data_connector_name
-      data_asset_name: {testing_file_name }
-      data_connector_query:
-        index: -1
-    expectation_suite_name: {expectation_suite_name }
-"""
-my_new_checkpoint_config = yaml.load(my_new_checkpoint_config)
+pattern = "yellow_tripdata_sample_*.csv"
+test_files=[]
+for file in files(datasource_path):
+    #if filename match pattern add to test_files
+    if fnmatch.fnmatch(file, pattern):
+        test_files.append(file)
 
-# NOTE: The following code (up to and including the assert) is only for testing and can be ignored by users.
-# In the current test, site_names are set to None because we do not want to update and build data_docs
-# If you would like to build data_docs then either remove `site_names=None` or pass in a list of site_names you would like to build the docs on.
-new_checkpoint = SimpleCheckpoint(
-    **my_new_checkpoint_config, data_context=context,
-)
-new_checkpoint_result = new_checkpoint.run()
-assert new_checkpoint_result.run_results
+for test_file in test_files:
+  # Create second checkpoint on testing file
+  testing_file_name = test_file
+  my_new_checkpoint_config = f"""
+  name: checkpoint on testing file
+  config_version: 1.0
+  class_name: SimpleCheckpoint
+  run_name_template: "%Y%m%d-%H%M%S-check-{testing_file_name }"
+  validations:
+    - batch_request:
+        datasource_name: {datasource_name }
+        data_connector_name: default_inferred_data_connector_name
+        data_asset_name: {testing_file_name }
+        data_connector_query:
+          index: -1
+        batch_spec_passthrough: 
+          reader_method: csv
+          reader_options: 
+            header: True
+            inferSchema: True
+      expectation_suite_name: {expectation_suite_name }
+  """
+  #above batch_spec_passthrough optional for header and schema
+  my_new_checkpoint_config = yaml.load(my_new_checkpoint_config)
+
+  # NOTE: The following code (up to and including the assert) is only for testing and can be ignored by users.
+  # In the current test, site_names are set to None because we do not want to update and build data_docs
+  # If you would like to build data_docs then either remove `site_names=None` or pass in a list of site_names you would like to build the docs on.
+  new_checkpoint = SimpleCheckpoint(
+      **my_new_checkpoint_config, data_context=context,
+  )
+  new_checkpoint_result = new_checkpoint.run()
+  assert new_checkpoint_result.run_results
 
 #save result to localsite html
 context.build_data_docs()
